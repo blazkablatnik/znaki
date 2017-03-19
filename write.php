fb058ceb1906a6fba3b480734c3a5aa6873b2880
Karin Frlic <kf5121@student.uni-lj.si>
2017-03-19 20:11:24 +0000

    Dodan script.js

--- write.php
+++ write.php
@@ -4,7 +4,10 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	    <title>Znaki</title>
-	    <link rel="stylesheet" href="style.css" type="text/css">
+	    <link rel="stylesheet" href="CSS/style.css" type="text/css">
+	    
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	    <script src="JavaScript/script.js"></script>
     </head>
     <body>
         <h1>Pisanje</h1>
